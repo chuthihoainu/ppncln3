88cf58668eb32c361647a0dc97f8cae829616589
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 07:54:44 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,36 +4,24 @@ st.write("VPANDAS xin chào ạ")
 import requests
 import nbformat
 from nbformat import read
-# Lấy URL của file Jupyter Notebook
-url = "https://github.com/chuthihoainu/ppncln2/blob/master/Main_RFM_3.ipynb"
+# Đọc file Jupyter Notebook
+notebook = read(url)
 
-# Gửi yêu cầu GET đến URL để lấy nội dung file Jupyter Notebook
-response = requests.get(url)
+# Hiển thị tiêu đề của file Jupyter Notebook
+st.title(notebook["metadata"]["title"])
 
-# Kiểm tra mã trạng thái
-if response.status_code == 200:
+# Duyệt qua các cell trong file Jupyter Notebook
+for cell in notebook["cells"]:
 
-    # Đọc nội dung file Jupyter Notebook
-    notebook = read(response.content)
+    # Kiểm tra loại cell
+    if cell["cell_type"] == "markdown":
 
-    # Hiển thị tiêu đề của file Jupyter Notebook
-    st.title(notebook["metadata"]["title"])
+        # Hiển thị nội dung markdown
+        st.markdown(cell["source"])
 
-    # Duyệt qua các cell trong file Jupyter Notebook
-    for cell in notebook["cells"]:
+    elif cell["cell_type"] == "code":
 
-        # Kiểm tra loại cell
-        if cell["cell_type"] == "markdown":
+        # Chạy mã code
+        exec(cell["source"])
 
-            # Hiển thị nội dung markdown
-            st.markdown(cell["source"])
 
-        elif cell["cell_type"] == "code":
-
-            # Chạy mã code
-            exec(cell["source"])
-
-else:
-
-    # Hiển thị thông báo lỗi
-    st.error("Lỗi khi lấy file Jupyter Notebook")
