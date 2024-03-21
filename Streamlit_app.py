b6a11f129e2fb232d6e87034fc1ef0c94c5de292
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 08:01:38 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,27 +1,25 @@
 import streamlit as st
 st.image("download.jpg")
 st.write("VPANDAS xin chào ạ")
+
 import requests
-import nbformat
 from nbformat import read
+
 # Đọc file Jupyter Notebook
+url = "url_to_your_notebook.ipynb"
 notebook = read(url)
 
 # Hiển thị tiêu đề của file Jupyter Notebook
-st.title(notebook["metadata"]["title"])
+st.title(notebook['metadata']['title'])
 
 # Duyệt qua các cell trong file Jupyter Notebook
-for cell in notebook["cells"]:
-
+for cell in notebook['cells']:
     # Kiểm tra loại cell
-    if cell["cell_type"] == "markdown":
-
+    if cell['cell_type'] == 'markdown':
         # Hiển thị nội dung markdown
-        st.markdown(cell["source"])
-
-    elif cell["cell_type"] == "code":
-
+        st.markdown(cell['source'])
+    elif cell['cell_type'] == 'code':
         # Chạy mã code
-        exec(cell["source"])
+        exec(cell['source'])
 
 
